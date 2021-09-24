823dd7d73433a0a0cb37f2a1965f5665189836c8
Farhan A <farhan7011@hotmail.com>
2021-09-24 23:30:09 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,6 @@
-try{
-  var express = require('express');
-  var bodyParser = require('body-parser');
-  var cors = require('cors');
-
-}catch(error){
-  console.error("ERROR are all the Dependencies installed?");
-  console.log(error);
-  process.exit(1);
-}
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var port = 3001;
 
@@ -16,6 +9,7 @@ var app = express();
 app.use(cors())
 
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(bodyParser.json());
 
 var sign_s3 = require('./controllers/sign_s3')
